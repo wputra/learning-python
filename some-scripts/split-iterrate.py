cd9c288cbc135004512ad8436765c9ee3da9123b
Wasis Adi Putranto <wasis.putra@gmail.com>
2020-10-21 13:38:05 +0800

    Update split-iterrate.py

--- some-scripts/split-iterrate.py
+++ some-scripts/split-iterrate.py
@@ -13,12 +13,20 @@ thisstring2 = "1F 2B 3D"
 for i in thislist:
     print(i)
 # list iterration from string
+thisdict3 = {}
+emptylist = []
 for i in range(len(thisstring)):
-    print(thisstring[i])
+    eli = thisstring[i]
+    if eli not in emptylist:
+        emptylist.append(eli)
+        thisdict3[eli] = thisstring.count(eli)
+print(emptylist)
+print(thisdict3)
 # dict iterration
+thisdict2 = {}
 for key, value in thisdict.items():
-    print(key)
-    print(value)
+    thisdict2[key] = list(str(value))
+print(thisdict2)
 # split space separated string to list
 splittolist = thisstring2.split()
 print(splittolist)
